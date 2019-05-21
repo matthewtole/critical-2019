71cf309b9ccd6ae46eb97b3990c0641cf4e21f2a
Matthew Tole <matthewtole@gmail.com>
2019-05-20 20:48:12 -0700

    Create empty Particle project folders

---
+++ wrist/src/wrist.cpp
@@ -0,0 +1,23 @@
+#include "application.h"
+#line 1 "/Users/matthewtole/Documents/Projects/critical-2019/wrist/src/wrist.ino"
+/*
+ * Project wrist
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/Users/matthewtole/Documents/Projects/critical-2019/wrist/src/wrist.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
